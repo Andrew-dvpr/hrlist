52f586db37155bdebdb5fe5f4bfddc31551f5caf
andrey_ry6alk0 <andrey.ry6alk0@gmail.com>
2020-11-12 15:21:55 +0300

    111

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+<header>
+      <h1>Vue.js SPA</h1>
+    </header>
+    <main>
+      <aside class="sidebar">
+      </aside>
+      <div class="content">
+      </div>
+    </main>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
